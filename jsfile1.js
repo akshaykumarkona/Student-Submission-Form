eab55ebacc89f78daaf99460bb0a9e357d8e903f
akshaykumarkona <118589209+akshaykumarkona@users.noreply.github.com>
2022-12-02 20:42:26 +0530

    Add files via upload

---
+++ jsfile1.js
@@ -0,0 +1,14 @@
+// document.getElementById('sumbit_btn').addEventListener("click",eagle)
+// function eagle(){
+//     console.log("Form Submitted");
+// //     var e=q.target
+// //     console.log(e)
+// // 
+// }
+
+document.getElementById("sumbit_btn").addEventListener("click",pressing);
+
+function pressing(){
+    location.href="file:///C:/Users/aksha/OneDrive/Desktop/Front-End/HTML%20SANDBOX%20FLE1/Sumission%20status.html";
+    console.log("Form Submitted");
+}
